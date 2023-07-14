d35a091afefa3c3eea7d7e16b438f4be2945639d
Szune <erikiwarson@gmail.com>
2023-07-14 22:22:03 +0200

    Add OpenSetOutfitDialogPacketHandler

---
+++ ArchivalTibiaV71WorldServer/PacketHandlers/OpenSetOutfitDialogPacketHandler.cs
@@ -0,0 +1,13 @@
+﻿using ArchivalTibiaV71WorldServer.Entities;
+using ArchivalTibiaV71WorldServer.Utilities;
+
+namespace ArchivalTibiaV71WorldServer.PacketHandlers
+{
+    public class OpenSetOutfitDialogPacketHandler : IPacketHandler
+    {
+        public void Handle(Player player, PacketReader reader)
+        {
+            player.Packets.SetOutfitDialog();
+        }
+    }
+}
