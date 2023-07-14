a0bb67a1633b324ae28068fd5f669a4514bf0e4d
Szune <erikiwarson@gmail.com>
2023-07-14 22:21:06 +0200

    Add ChangeOutfitPacketHandler

---
+++ ArchivalTibiaV71WorldServer/PacketHandlers/ChangeOutfitPacketHandler.cs
@@ -0,0 +1,21 @@
+﻿using ArchivalTibiaV71WorldServer.Constants;
+using ArchivalTibiaV71WorldServer.Entities;
+using ArchivalTibiaV71WorldServer.Utilities;
+
+namespace ArchivalTibiaV71WorldServer.PacketHandlers
+{
+    public class ChangeOutfitPacketHandler : IPacketHandler
+    {
+        public void Handle(Player player, PacketReader reader)
+        {
+            var outfitId = reader.ReadU8();
+            var head = reader.ReadU8();
+            var body = reader.ReadU8();
+            var legs = reader.ReadU8();
+            var feet = reader.ReadU8();
+
+            player.Outfit.Set((Outfits)outfitId, head, body, legs, feet);
+            player.Packets.Creature.UpdateOutfit(player);
+        }
+    }
+}
